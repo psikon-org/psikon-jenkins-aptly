fa35470d9ccc444d91f57da1736f8ef336865f8c
Alex Dow <adow@psikon.com>
2020-04-02 16:44:45 -0400

    first commit

---
+++ vars/publishDeb.groovy
@@ -0,0 +1,20 @@
+def call(Map config) {
+
+  def component = config.get("component", "");
+  def distribution = config.get("distribution", "");
+  def debFile = config.get("debFile");
+  def repoName = config.get("repoName");
+
+  sh """
+    EXISTS=$(aptly repo list --raw | grep "${repoName}")
+    if [ -z "$EXISTS" ]
+    then
+        aptly repo create -component="${component}" "${repoName}"
+        aptly publish repo -distribution="${distribution}" -component="${component}" "${repoName}"
+    fi
+
+    aptly repo add "${repoName}" "${debFile}"
+
+    aptly publish update "${distribution}"
+  """
+}
